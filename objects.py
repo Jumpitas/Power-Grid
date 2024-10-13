c06442fc32aab0a98f053b47dff3953952b7e339
rafa907p <rafa907ak@gmail.com>
2024-10-13 17:41:09 +0100

    PowerPlantMarket and ResourceMarket
    Added both markets. ResourceMarket fully functional, PowerPlantMarket missing auction.
    
    PowerPlant deck fully functional.
    
    Added some rules, included initial PowerPlant and Resource markets setup.

--- objects.py
+++ objects.py
@@ -5,6 +5,7 @@
 # - hybrid são as que usam mais do que 1 resource
 # - restrições são colocadas aos agentes na parte de possible moves
 
+import random
 
 class PowerPlant:
     def __init__(self, min_bid, cities, resource_type=[], resource_num=0, is_hybrid=False, is_step=False):
@@ -35,37 +36,37 @@ class PowerPlant:
         plant2.available_storage -= 1
 
     # Nota: os resources usados voltam para o market!!!
-    # power_on deve também dar return de replenish_market para 
-    # repor o mercado corretamente
+    # power_on deve também dar return de replenish_bank para 
+    # repor o resource bank corretamente (nº de peças available importa)
 
     def power_on(self):
         # se o nº de resources in storage for igual ao necessário
         # para ligar power então é automático
         if (sum(self.storage.values()) == self.resource_num):
-            replenish_market = self.storage
+            replenish_bank = self.storage
             self.storage = {key: 0 for key in self.storage}
             self.available_storage += self.resource_num
-            return True
+            return replenish_bank
 
         # se for maior que o necessário e não híbrido
         # (não engloba menor que necessário porque se é menor não pode ligar)
         elif not self.is_hybrid:
-            replenish_market = {self.resource_type[0]: self.resource_num}
+            replenish_bank = {self.resource_type[0]: self.resource_num}
             self.storage[self.resource_type[0]] -= self.resource_num
             self.available_storage += self.resource_num
-            return True
+            return replenish_bank
 
 
         # caso contrário, tem de ser capaz de escolher
         # a combinação de recursos que quer utilizar
         # enquanto não escolher suficientes, faz um ciclo
         else:
-            replenish_market = {key: 0 for key in self.storage}
+            replenish_bank = {key: 0 for key in self.storage}
             counter = 0
             while counter < self.resource_num:
                 for rtype in self.resource_type:
                     choice = int(input(f"How many of type {rtype} do you wish to consume? "))
-                    replenish_market[rtype] += choice
+                    replenish_bank[rtype] += choice
                     self.storage[rtype] -= choice
                     counter += choice
 
@@ -73,18 +74,14 @@ class PowerPlant:
                         print("Error: Consumed more resources than needed!!! ")
                     
                     elif counter == self.resource_num:
-                        return True
-        return False
+                        return replenish_bank
+        return None
 
 
-# Baralho de cartas originals
+# Baralho de cartas original
 
-power_plant_deck = [
+power_plant_socket = [
     # Coal Plants
-    PowerPlant(4,1,["coal"],2),
-    PowerPlant(8,2,["coal"],3),
-    PowerPlant(10,2,["coal"],2),
-    PowerPlant(15,3,["coal"],2),
     PowerPlant(20,5,["coal"],3),
     PowerPlant(25,5,["coal"],2),
     PowerPlant(31,6,["coal"],3),
@@ -92,9 +89,6 @@ power_plant_deck = [
     PowerPlant(42,6,["coal"],2),
 
     # Oil Plants
-    PowerPlant(3,1,["oil"],2),
-    PowerPlant(7,2,["oil"],3),
-    PowerPlant(9,1,["oil"],1),
     PowerPlant(16,3,["oil"],2),
     PowerPlant(26,5,["oil"],2),
     PowerPlant(32,6,["oil"],3),
@@ -102,15 +96,12 @@ power_plant_deck = [
     PowerPlant(40,6,["oil"],2),
 
     # Garbage Plants
-    PowerPlant(6,1,["garbage"],1),
-    PowerPlant(14,2,["garbage"],2),
     PowerPlant(19,3,["garbage"],2),
     PowerPlant(24,4,["garbage"],2),
     PowerPlant(30,6,["garbage"],3),
     PowerPlant(38,7,["garbage"],3),
 
     # Uranium Plants
-    PowerPlant(11,2,["uranium"],1),
     PowerPlant(17,2,["uranium"],1),
     PowerPlant(23,3,["uranium"],1),
     PowerPlant(28,4,["uranium"],1),
@@ -118,14 +109,11 @@ power_plant_deck = [
     PowerPlant(39,6,["uranium"],1),
 
     # Hybrid Plants
-    PowerPlant(5,1,["coal","oil"],2,True),
-    PowerPlant(12,2,["coal","oil"],2,True),
     PowerPlant(21,4,["coal","oil"],2,True),
     PowerPlant(29,4,["coal","oil"],1,True),
     PowerPlant(46,7,["coal","oil"],3,True),
 
     # Eco Plants
-    PowerPlant(13,1),
     PowerPlant(18,2),
     PowerPlant(22,2),
     PowerPlant(27,3),
@@ -139,3 +127,125 @@ power_plant_deck = [
     # Step3 Card
     PowerPlant(99,99,is_step=True)
 ]
+
+power_plant_plug = [
+    # Coal Plants
+    PowerPlant(4,1,["coal"],2),
+    PowerPlant(8,2,["coal"],3),
+    PowerPlant(10,2,["coal"],2),
+    PowerPlant(15,3,["coal"],2),
+
+    # Oil Plants
+    PowerPlant(3,1,["oil"],2),
+    PowerPlant(7,2,["oil"],3),
+    PowerPlant(9,1,["oil"],1),
+
+    # Garbage Plants
+    PowerPlant(6,1,["garbage"],1),
+    PowerPlant(14,2,["garbage"],2),
+
+    # Uranium Plants
+    PowerPlant(11,2,["uranium"],1),
+
+    # Hybrid Plants
+    PowerPlant(5,1,["coal","oil"],2,True),
+    PowerPlant(12,2,["coal","oil"],2,True),
+
+    # Eco Plants
+    PowerPlant(13,1)
+]
+
+
+
+# Resources só são repostas se houver suficientes no "Resource Bank"
+# resource bank representa o número de peças de madeira disponíveis
+# só é possível repôr o market se existirem peças suficientes
+
+from rule_tables import price_table, resource_replenishment, remove_cards
+
+resource_bank = {
+    "coal": 24,
+    "oil": 24,
+    "garbage": 24,
+    "uranium": 12
+}
+
+
+class ResourceMarket:
+    def __init__(self, coal, oil, garbage, uranium):
+        self.max = {"coal": 24,"oil": 24,
+                    "garbage": 24,"uranium": 12} 
+        # quantidades atuais de recursos no mercado
+        self.in_market = {"coal":coal, "oil":oil,
+                          "garbage":garbage, "uranium":uranium}
+
+    def resource_price(self, rtype):
+        # Se não houver disponível
+        if self.in_market[rtype]==0: return None
+
+        if rtype == "uranium": return price_table[rtype][self.in_market["uranium"]]
+        else: 
+            for key, price in price_table[rtype].items():
+                if self.in_market[str(rtype)] in key:
+                    return price
+                
+    # restrição de agente: só compra se tiver dinheiro e puder guardar
+    def purchase_batch(self, rtype, rnum):
+        if self.in_market[rtype] >= rnum:
+            self.in_market[rtype] -= rnum
+            return True
+        return False
+    
+    def refill_market(self, step, nplayer):
+        fillup = resource_replenishment[step][nplayer]
+        for rtype, rnum in fillup:
+            # há >= peças do que necessário e espaço disponível para o fazer
+            if (resource_bank[rtype] >= rnum) and (self.in_market[rtype] + rnum <= self.max[rtype]):
+                resource_bank[rtype] -= rnum
+                self.in_market[rtype] += rnum
+            # há >= peças do que necessário mas espaço menor do que o replenish
+            elif (resource_bank[rtype] >= rnum):
+                diff = self.max[rtype] - self.in_market[rtype]
+                resource_bank[rtype] -= diff
+                self.in_market[rtype] += diff
+            # há <= peças do que necessário mas espaço suficiente para o replenish
+            elif (self.in_market[rtype] + rnum <= self.max[rtype]):
+                self.in_market[rtype] += resource_bank[rtype]
+                resource_bank[rtype] = 0
+
+    # refill das pecinhas
+    def refill_bank(self, refill):
+        for rtype, rnum in refill:
+            resource_bank[rtype] += rnum
+
+
+class PowerPlantMarket:
+    def __init__(self):
+        self.plug = power_plant_plug
+        self.socket = power_plant_socket
+        self.market = []
+        self.deck = []
+        #random.shuffle(self.deck)
+
+    def setup(self, nplayers):
+        # 8 in rulebook
+        random.shuffle(self.plug)
+        self.market = self.plug[:8]
+        self.plug = self.plug[8:]
+        self.market.sort(key=lambda plant: plant.min_bid)
+        plug_card = [self.plug.pop()]
+
+        # 9 in rulebook
+        set_aside = remove_cards[nplayers]
+        step3 = [self.socket.pop()]
+        random.shuffle(self.socket)
+        box = self.plug[:set_aside[0]] + self.socket[:set_aside[1]]
+        self.plug = self.plug[set_aside[0]:]
+        self.socket = self.socket[set_aside[1]:]
+
+        # agora que vai deixar de haver separação, distinguir cartas usando back_of_card
+        # topo do deck é index 0 e bottom é -1
+        self.deck = self.socket + self.plug
+        random.shuffle(self.deck)
+        self.deck = plug_card + self.deck + step3
+
--- rule_tables.py
+++ rule_tables.py
@@ -1,13 +1,11 @@
 # rule_tables.py
 
 # Income table: Elektro earned based on the number of cities powered
-city_cashback = {
-    0: 10, 1: 22, 2: 33, 3: 44, 4: 54,
-    5: 64, 6: 73, 7: 82, 8: 90, 9: 98,
-    10: 105, 11: 112, 12: 118, 13: 124,
-    14: 129, 15: 134, 16: 138, 17: 142,
-    18: 145, 19: 148, 20: 150
-}
+city_cashback=[10,22,33,44,54,
+               64,73,82,90,98,
+               105,112,118,124,
+               129,134,138,142,
+               145,148,150]
 
 # Resource replenishment table: Resources added to the market at the end of each turn
 # Step: {Number of Players: {Resource: Amount}}
@@ -32,7 +30,25 @@ resource_replenishment = {
         4: {'coal': 5, 'oil': 6, 'garbage': 5, 'uranium': 3},
         5: {'coal': 6, 'oil': 7, 'garbage': 6, 'uranium': 4},
         6: {'coal': 7, 'oil': 9, 'garbage': 6, 'uranium': 5},
-    },
+    }
+}
+
+
+price_table = {
+    "uranium": {1:16, 2:14, 3:12, 4:10, 
+                  5:8, 6:7, 7:6, 8:5, 
+                  9:4, 10:3, 11:2, 12:1},
+    
+    ("coal","oil","garbage"): {
+        (1,2,3): 8,
+        (4,5,6): 7,
+        (7,8,9): 6,
+        (10,11,12): 5,
+        (13,14,15): 4,
+        (16,17,18): 3,
+        (19,20,21): 2,
+        (22,23,24): 1
+    }
 }
 
 # Number of connected cities required to start Step 2
@@ -50,20 +66,27 @@ game_end_cities = {
     3: 17,
     4: 17,
     5: 15,
-    6: 14,
+    6: 14
 }
 
-# Maximum resource quantities in the market
-resource_max_quantities = {
-    'coal': 24,
-    'oil': 24,
-    'garbage': 24,
-    'uranium': 12,
-}
 
 # Building cost table: Cost to build a house based on the current step
 building_cost = {
     1: 10,  # Step 1
     2: 15,  # Step 2
-    3: 20,  # Step 3
+    3: 20  # Step 3
 }
+
+
+remove_cards = {
+    # Number of players: (plug,socket)
+    2: (1,5),
+    3: (2,6),
+    4: (1,3),
+    5: (0,0),
+    6: (0,0)
+}
+
+def back_of_card(plant):
+    if 3 <= plant.min_bid <= 15: return "plug"
+    return "socket"
